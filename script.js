e102b4f491bcb53bfd8ee7b92eb4201aec513d2f
Yasmin Devegili <48127784+yasmindevegili@users.noreply.github.com>
2023-05-18 11:44:29 -0300

    Second Commit

--- script.js
+++ script.js
@@ -102,4 +102,123 @@ const dog = {
 const json = JSON.stringify(dog)
 console.log(json)
 
-const obj = JSON.parse
+const obj = JSON.parse(json)
+console.log(obj)
+
+//Estruturas condicionais
+const a = 10
+
+if(a > 8) {
+    console.log('A é maior que 8')
+}
+
+const b = "Yasmin"
+
+if(b === "João") { //triple equal = validação de tipo e valor
+    console.log('O nome é João')
+}else{
+    console.log('O nome é ' + b)
+}
+
+const n = 14
+
+//if ternário
+let teste = n < 20 ? 'Yes' : 'No'
+console.log(teste)
+
+//Estruturas de repetição
+const myList = [1, 2, 4, 5]
+let counter = 0
+
+while(counter < myList.length){
+    console.log(myList[counter])
+    counter++
+}
+
+const mySecondList = ['a', 'b', 'c']
+
+for(let counter = 0; counter < mySecondList.length; counter++){
+    console.log(`Imprimindo: ${mySecondList[counter]}`) //template literals
+}
+
+//Métodos de array -> repetição
+const names = ['Yasmin', 'Maria', 'Pedro']
+names.forEach(function(name){
+    console.log(`O nome é: ${name}`)
+})
+
+//Métodos de array -> modificação
+const modifiedNames = names.map(function(name){
+    if(name === 'Yasmin'){
+        return (name = 'Sra. Yasmin')
+    }else{
+        return name
+    }
+})
+
+console.log(modifiedNames)
+
+const bigNumbers = [1, 3, 5, 6 , 7].filter(function(number){
+    return number >= 5
+})
+
+console.log(bigNumbers)
+
+const sumAll = [10, 30, 50].reduce(function(total, number){
+    return total + number
+})
+
+console.log(sumAll)
+
+//Funções
+function minhaFuncao(){
+    console.log("Olá")
+}
+
+minhaFuncao()
+
+
+//Arrow function
+const myArrow = (a, b) => {
+    return a + b
+}
+
+console.log(myArrow(2, 5))
+
+//Classes
+class Product {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+
+    productDetails() {
+        return `O nome do produto é ${this.name} e o preço é ${this.price}`
+    }
+}
+
+const socks = new Product('Meia Branca', 10.99)
+const shirt = new Product('Camisa preta', 22.99)
+
+console.log(socks.name)
+console.log(socks.price)
+console.log(shirt.name)
+console.log(shirt.productDetails())
+
+//herança
+class SuperProduct extends Product{
+    constructor(name, price, size) {
+        super(name, price)
+        this.size = size
+    }
+
+    //static
+    static sayHello(){
+        console.log('Hello')
+    }
+    
+}
+
+const tenis = new SuperProduct('Tenis Vermenlho', 59.00, '43')
+console.log(tenis)
+SuperProduct.sayHello()
